c8d07aab582c83a3f3c3dfa8c346a24f3705b0cf
kit0-0 <kt1767@gmail.com>
2023-08-01 15:36:31 +0530

    use dispach to store

--- src/components/mission/MissionsList.js
+++ src/components/mission/MissionsList.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Table } from 'react-bootstrap';
+import { getMissions } from '../../redux/mission/missions';
+import MissionItem from './MissionItem';
 
-const MissionsList = () => (
-  <Table striped bordered responsive>
-    <thead>
-      <tr>
-        <th className="fw-bold">Mission</th>
-        <th>Description</th>
-        <th>Status</th>
-        <th className="text-center">Join/Leave</th>
-      </tr>
-    </thead>
-    <tbody />
-  </Table>
-);
+const MissionsList = () => {
+  const dispatch = useDispatch();
+  const missionsList = useSelector((state) => state.missions);
+
+  useEffect(() => {
+    dispatch(getMissions());
+  }, [dispatch]);
+
+  return (
+    <Table striped bordered responsive>
+      <thead>
+        <tr>
+          <th className="fw-bold">Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th className="text-center">Join/Leave</th>
+        </tr>
+      </thead>
+      <tbody>
+        {missionsList.map((mission) => (
+          <MissionItem key={mission.mission_id} mission={mission} />
+        ))}
+      </tbody>
+    </Table>
+  );
+};
 
 export default MissionsList;
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,11 @@ import { legacy_createStore as createStore, combineReducers, applyMiddleware } f
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import reducerRockets from './rockets/rocketsReducer';
+import missionsReducer from './mission/fetchMissions';
 
 const rootReducer = combineReducers({
   Rockets: reducerRockets,
+  missions: missionsReducer,
 });
 
 const store = createStore(
